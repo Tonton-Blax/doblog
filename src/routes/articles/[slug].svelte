0919fcd2b4eb7cefe86cc36248187df93e7ec57a
Tonton-Blax <redval@gmail.com>
2021-09-24 00:17:00 +0200

    hop

--- src/routes/articles/[slug].svelte
+++ src/routes/articles/[slug].svelte
@@ -44,47 +44,6 @@
   
 		<div>
 		  <div class="relative pt-6 px-4 sm:px-6 lg:px-8" />
-			  
-		  <!--
-			Mobile menu, show/hide based on menu open state.
-  
-			Entering: "duration-150 ease-out"
-			  From: "opacity-0 scale-95"
-			  To: "opacity-100 scale-100"
-			Leaving: "duration-100 ease-in"
-			  From: "opacity-100 scale-100"
-			  To: "opacity-0 scale-95"
-		  -->
-		  <div class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-			<div class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-			  <div class="px-5 pt-4 flex items-center justify-between">
-				<div>
-				  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
-				</div>
-				<div class="-mr-2">
-				  <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-					<span class="sr-only">Close main menu</span>
-					<!-- Heroicon name: outline/x -->
-					<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-					  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				  </button>
-				</div>
-			  </div>
-			  <div class="px-2 pt-2 pb-3 space-y-1">
-				<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Product</a>
-  
-				<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Features</a>
-  
-				<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Marketplace</a>
-  
-				<a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50">Company</a>
-			  </div>
-			  <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
-				Log in
-			  </a>
-			</div>
-		  </div>
 		</div>
   
 		<main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
@@ -94,21 +53,14 @@
 			  <span class="block text-indigo-600 xl:inline">{post.h1.split(' ').filter((_,i) => i >= 4).join(' ')}</span>
 			</h1>
 			<p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-			  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+			  {post.contenu[0].chapocontent}
 			</p>
 			<div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
 			  <div class="rounded-md shadow">
-				<a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+				<a href="/about" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
 				  Contactez-moi wesh
 				</a>
 			  </div>
-			  <!--
-			  <div class="mt-3 sm:mt-0 sm:ml-3">
-				<a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
-				  Live demo
-				</a>
-			  </div>
-			  -->
 			</div>
 		  </div>
 		</main>
@@ -120,18 +72,18 @@
   </div>
 
   
-  <div class="container px-6 py-16 mx-auto">
+  <div class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
 	<div class="items-center lg:flex">
 		<div class="w-full lg:w-1/2">
 			<div class="lg:max-w-lg">
-				<h2 class="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl">{post.contenu[1].h2}</h2>
-				<p class="mt-2 text-gray-600 dark:text-gray-400">{@html post.contenu[2].body}</p>
-				<button class="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-indigo-600 rounded-md lg:w-auto hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Shop Now</button>
+				<h2 class="text-2xl font-semibold text-indigo-600 dark:text-white lg:text-4xl">{post.contenu[1].h2}</h2>
+				<p class="mt-2 text-gray-600 dark:text-gray-400 leading-relaxed">{@html post.contenu[2].body}</p>
+				<!-- <button class="w-full px-3 py-2 mt-6 text-xs font-medium text-white uppercase transition-colors duration-200 transform bg-indigo-600 rounded-md lg:w-auto hover:bg-indigo-500 focus:outline-none focus:bg-indigo-500">Shop Now</button> -->
 			</div>
 		</div>
 
 		<div class="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-			<img class="w-full h-full lg:max-w-2xl" src="{post.thumbnail}" alt="Catalogue-pana.svg">
+			<img class="w-full h-full lg:max-w-2xl pl-8" src="{post.contenu[3].images[0]}" alt="Catalogue-pana.svg">
 		</div>
 	</div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,11 @@ const config = {
 		"./src/**/*.{html,js,svelte,ts}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: { 
+				sans: ["Poppins"] 
+			}
+		},
 	},
 	plugins: [],
 };
