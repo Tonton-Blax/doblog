487d605f88260921ec377abed859931815b31de3
Tonton-Blax <redval@gmail.com>
2021-09-26 23:19:27 +0200

    hop

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,16 +1,11 @@
 <script>
-	import { navigating, page } from '$app/stores';
+	import { page } from '$app/stores';
+	import { slide } from 'svelte/transition';
 	import {onMount} from 'svelte'
 	import Dorothee from './Dorothee.svelte'
 	let ready = false;
-		
+	let open = false;
 	onMount(()=>setTimeout( () => ready = true , 300));
-	/*
-	$: if ($navigating!==null) {
-		ready = false;
-		setTimeout( () => ready = true , 1000);
-	}
-	*/
 
 </script>
 
@@ -44,12 +39,14 @@
 		  </div>
 		</div>
 		{#if ready}
-						<Dorothee/>
-				{/if}
+			<Dorothee/>
+		{/if}
 
 		<div class="-mr-2 flex items-center sm:hidden">
 		  <!-- Mobile menu button -->
-		  <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
+		  <button 
+		  	on:click={()=>open=!open}
+		 	type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
 			<span class="sr-only">Open main menu</span>
 			<!--
 			  Icon when menu is closed.
@@ -58,18 +55,13 @@
   
 			  Menu open: "hidden", Menu closed: "block"
 			-->
+			
 			<svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-			</svg>
-			<!--
-			  Icon when menu is open.
-  
-			  Heroicon name: outline/x
-  
-			  Menu open: "block", Menu closed: "hidden"
-			-->
-			<svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+			{#if open}
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+			{:else}
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+			{/if}
 			</svg>
 		  </button>
 		</div>
@@ -77,37 +69,16 @@
 	</div>
   
 	<!-- Mobile menu, show/hide based on menu state. -->
-	<div class="sm:hidden" id="mobile-menu">
+	{#if open}
+	<div class="sm:hidden" id="mobile-menu" transition:slide>
 	  <div class="pt-2 pb-3 space-y-1">
 		<!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" -->
 		<a href="/" class:active={$page.path === '/'} sveltekit:prefetch class="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Accueil</a>
 		<a href="/about" class:active={$page.path === '/about'} class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Me Myself & I</a>
 		<a href="/articles" class:active={$page.path === '/articles'} sveltekit:prefetch={!$page.path.includes('articles')} class="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium">Articles</a>
 	  </div>
-	  <div class="pt-4 pb-3 border-t border-gray-200">
-		<div class="flex items-center px-4">
-		  <div class="flex-shrink-0">
-			<img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-		  </div>
-		  <div class="ml-3">
-			<div class="text-base font-medium text-gray-800">Tom Cook</div>
-			<div class="text-sm font-medium text-gray-500">tom@example.com</div>
-		  </div>
-		  <button type="button" class="ml-auto flex-shrink-0 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-			<span class="sr-only">View notifications</span>
-			<!-- Heroicon name: outline/bell -->
-			<svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-			  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-			</svg>
-		  </button>
-		</div>
-		<div class="mt-3 space-y-1">
-		  <a href="/" class:active={$page.path === '/'} sveltekit:prefetch class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Accueil</a>
-		  <a href="/about" class:active={$page.path === '/about'} class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Me Myself & I</a>
-		  <a href="/articles" class:active={$page.path === '/articles'} sveltekit:prefetch={!$page.path.includes('articles')} class="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100">Articles</a>
-		</div>
-	  </div>
 	</div>
+	{/if}
   </nav>
   
 
